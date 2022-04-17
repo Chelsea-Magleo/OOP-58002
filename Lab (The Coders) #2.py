07af6951b890593f9e4f3bea08e0373fe9c11fd4
Chelsea-Magleo <98940276+Chelsea-Magleo@users.noreply.github.com>
2022-04-17 21:48:36 +0800

    Add files via upload

--- Lab (The Coders) #2.py
+++ Lab (The Coders) #2.py
@@ -1,5 +1,5 @@
 from tkinter import*
-class MyWindow:
+class TextField:
     def __init__(self, win):
         self.t1 = Entry()
         self.t1.place(x=130, y=50)
@@ -8,7 +8,7 @@ class MyWindow:
 
 
 window = Tk()
-mywin = MyWindow(window)
-window.title('Text field (The coders)')
+mywin = TextField(window)
+window.title("Text field (The coders)")
 window.geometry("400x300+10+10")
 window.mainloop()
